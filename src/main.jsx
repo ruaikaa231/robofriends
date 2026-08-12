296df9f1fdd5e1f899054a445bf4b53d700dd49a
Agnes <agnesruaika2@gmail.com>
2026-08-12 13:59:45 -0500

    working on robo website send 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Card from './Card.jsx';
+import App from './App';
 import 'tachyons';
 import { robots } from './robots';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <App />
   </StrictMode>,
 )
