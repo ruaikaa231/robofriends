1c1c04b9f49a26a1d7e6c118947e704c54606eba
Agnes <agnesruaika2@gmail.com>
2026-08-13 13:07:55 -0500

    finalizing 3

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox.jsx';
-import Scroll from './Scroll.jsx';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox.jsx';
+import Scroll from '../components/Scroll.jsx';
 import './App.css'
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Card from './Card.jsx';
-import App from './App';
+import Card from './components/Card.jsx';
+import App from './containers/App';
 import 'tachyons';
 import { robots } from './robots';
 
